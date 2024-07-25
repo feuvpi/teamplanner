85edf511d9d7a38b9420bf6edebba9c3469b2820
feuvpi <fredvpg@gmail.com>
2024-07-25 00:07:44 -0300

    sidebar working properly

--- src/lib/components/AppLayout/AppHeader.svelte
+++ src/lib/components/AppLayout/AppHeader.svelte
@@ -47,6 +47,38 @@
 			isOpenDropdown = false;
 		}
 	};
+
+	// function expandSidebar() {
+	//     const sidebar = document.getElementById('sidebar');
+	//     const mainContent = document.querySelector('.ml-16');
+
+	//     if (sidebar.style.width === '16rem') {
+	//         sidebar.style.width = '4rem';
+	//         mainContent.style.marginLeft = '4rem';
+	//         sidebar.classList.remove('text-left', 'px-6');
+	//         sidebar.classList.add('text-center', 'px-0');
+	//     } else {
+	//         sidebar.style.width = '16rem';
+	//         mainContent.style.marginLeft = '16rem';
+	//         sidebar.classList.add('text-left', 'px-6');
+	//         sidebar.classList.remove('text-center', 'px-0');
+	//     }
+
+	//     const labels = sidebar.querySelectorAll('span');
+	//     labels.forEach(label => label.classList.toggle('opacity-0'));
+	// }
+
+	// function highlightSidebarItem(element) {
+	// const buttons = document.querySelectorAll("#sidebar button");
+	// buttons.forEach(btn => {
+	//     btn.classList.remove('bg-gradient-to-r', 'from-cyan-400', 'to-cyan-500', 'text-white', 'w-48', 'ml-0');
+	//     btn.firstChild.nextSibling.classList.remove('text-white');
+	// });
+	// element.classList.add('bg-gradient-to-r', 'from-cyan-400', 'to-cyan-500', 'w-56', 'h-10','ml-0');
+	// element.firstChild.nextSibling.classList.add('text-white');
+	// }
+
+	function handleSidebar() {}
 </script>
 
 <header
@@ -62,7 +94,7 @@
 			aria-expanded="false"
 			aria-label="Toggle sidenav"
 			on:click={openSidebar}
-			class="text-4xl pr-4 focus:outline-none lg:visible text-light-text dark:text-dark-text"
+			class="text-4xl pr-4 focus:outline-none lg:visible text-light-text dark:text-dark-textbar"
 		>
 			&#8801;
 		</button>
